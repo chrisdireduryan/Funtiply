c6cbcc0aef07faa5e0a39e544a4720e41e60857c
Chris Direduryan <direduryan@gmail.com>
2020-12-25 14:41:58 +0300

    first-commit

---
+++ Funtiply/FuntiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuntiplyApp.swift
+//  Funtiply
+//
+//  Created by Chris Direduryan on 25.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct FuntiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
