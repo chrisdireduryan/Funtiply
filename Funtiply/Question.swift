6d9c4636a3717e33b14e8e536f7790f6f259ee45
Chris Direduryan <direduryan@gmail.com>
2020-12-26 17:30:50 +0300

    Question struct added

---
+++ Funtiply/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Funtiply
+//
+//  Created by Chris Direduryan on 26.12.2020.
+//
+
+import Foundation
+
+struct Question {
+    let id: Int
+    let question: String
+    let answer: Int
+}
